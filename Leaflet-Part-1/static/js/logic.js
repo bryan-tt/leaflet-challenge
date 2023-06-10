58d73740fc7f637abf7b93e34d78f8b630527588
bryan-tt <bryantsan916@gmail.com>
2023-06-10 02:45:09 -0700

    Added Legend

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -5,10 +5,10 @@ d3.json(geourl).then(function (data) {
     createFeatures(data.features);
 });
 
+let earthquakes;
+
 // Define a function that we want to run once for each feature in the features array.
 function createFeatures(earthquakeData) {
-    
-
     // Function to choose RGB color based on depth (coordinate 3)
     function chooseColor(depth) {
         if (depth > 90) {
@@ -18,13 +18,13 @@ function createFeatures(earthquakeData) {
         } else if (depth >= 50) {
             return "rgb(255, 165, 0)"; // Orange
         } else if (depth >= 30) {
-            return "rgb(255, 244, 0)"; // Yellow 
+            return "rgb(255, 244, 0)"; // Yellow
         } else if (depth >= 10) {
             return "rgb(173,255,47)"; // Lime Green
         } else {
             return "rgb(0, 255, 0)"; // Green
         }
-    };
+    }
 
     // Change point to circle
     function pointToLayer(feature, latlng) {
@@ -32,25 +32,27 @@ function createFeatures(earthquakeData) {
         return L.circleMarker(latlng, {
             radius: feature.properties.mag * 4,
             fillColor: chooseColor(feature.geometry.coordinates[2]),
-            color: 'black',
+            color: "black",
             weight: 0.5,
             opacity: 1,
             fillOpacity: 0.7,
         });
     }
 
-    // Give each feature a popup that describes the place and time of the earthquake.
+    // Give each feature a popup that describes the place, magnitude, and depth of the earthquake.
     function onEachFeature(feature, layer) {
         layer.bindPopup(
-            `<h3>${feature.properties.place}</h3><hr><p>${new Date(
-                feature.properties.time
-            )}</p>`
+            `<h3>${
+                feature.properties.place
+            }</h3><hr><p>Magnitude: ${feature.properties.mag.toFixed(
+                2
+            )}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`
         );
     }
 
     // Create a GeoJSON layer that contains the features array on the earthquakeData object.
     // Run the onEachFeature function once for each piece of data in the array.
-    let earthquakes = L.geoJSON(earthquakeData, {
+    earthquakes = L.geoJSON(earthquakeData, {
         pointToLayer: pointToLayer,
         onEachFeature: onEachFeature,
     });
@@ -77,7 +79,7 @@ function createMap(earthquakes) {
 
     // Create a baseMaps object
     let baseMaps = {
-        "Street Map": stadia,
+        "Grayscale": stadia,
         "Topographic Map": topo,
     };
 
@@ -94,11 +96,50 @@ function createMap(earthquakes) {
     });
 
     // Create a layer control.
-    // Pass it our baseMaps and overlayMaps.
+    // Pass in baseMaps and overlayMaps.
     // Add the layer control to the map.
     L.control
         .layers(baseMaps, overlayMaps, {
             collapsed: false,
         })
         .addTo(myMap);
+
+    // Set up the legend.
+    let legend = L.control({ position: "bottomright" });
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend");
+        let limits = [
+            "-10 to 10",
+            "10 to 30",
+            "30 to 50",
+            "50 to 70",
+            "70 to 90",
+            "90+",
+        ];
+        let colors = [
+            "rgb(0, 255, 0)",
+            "rgb(173,255,47)",
+            "rgb(255, 244, 0)",
+            "rgb(255, 165, 0)",
+            "rgb(255, 69, 0)",
+            "rgb(255, 0, 0)",
+        ];
+        let labels = ["<strong>Depth of<br>Earthquake</strong>"];
+
+        limits.forEach(function (limit, index) {
+            labels.push(
+                '<li style="background-color: ' +
+                    colors[index] +
+                    '">' +
+                    limits[index] +
+                    "</li>"
+            );
+        });
+
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+
+    // Adding the legend to the map
+    legend.addTo(myMap);
 }
